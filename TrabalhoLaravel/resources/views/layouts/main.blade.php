c71d380c548329c28bccc3a051857baa56f4283b
Gabriel Candeias Barbosa <gabriel.barbosa.03@edu.unipar.br>
2024-06-03 23:02:55 -0300

    fix: mudança dos resources

--- TrabalhoLaravel/resources/views/layouts/main.blade.php
+++ TrabalhoLaravel/resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="/resources/css/app.css">
-    <script src="/resources/js/app.js" defer type="text/javascript"></script>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <script src="/js/app.js" defer type="text/javascript"></script>
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
@@ -14,7 +14,7 @@
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../../js/fontawesome-free-5.15.4-web/css/all.css">
-    <script src="../../js/js/fontawesome-free-5.15.4-web/js/all.js"></script>
+    <script src="/js/fontawesome-free-5.15.4-web/js/all.js"></script>
     <title>System</title>
 </head>
 
